96a42f89bc0f7c2d32e17eac1b5bf5772325c2d8
Karan Marwaha <karan11355@gmail.com>
2025-02-14 18:07:54 -0500

    feacture1

--- miniproject.py
+++ miniproject.py
@@ -6,18 +6,28 @@ studentcourses=[]
 def decision():
     dec=int(input("would you like to view (1), display(2) ,register (3), or exit (4)"))
     return dec
+
               
-def check():
+def view():
+    print(courses)
+    
+def register():
+    print(courses)
+    for i in range(0,3):
+        user_class = input("Enter course to add(MAX OF THREE")
+        studentcourses.append(user_class)
+        
+        
+        
+    
     
 def main():
    
     while True:
         try:
-            
-            check()
             if decision()==4:
                 break
-            elif deicion()==1:
+            elif decision()==1:
                 view()
             elif decision==2:
                 display()
