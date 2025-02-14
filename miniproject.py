f63ac360bb3b4a9f42556b5fbca1e9fa4e501ac4
Ahmed Abuelbasher <abuelbasheribrahim.a@northeastern.edu>
2025-02-14 17:56:35 -0500

    framework

--- miniproject.py
+++ miniproject.py
@@ -1,8 +1,38 @@
-def check():
-    print("h")
 
+courses=['Calculus','Cornerstone','Chemistry', 'Physics']
+studentcourses=[]
+
+
+def decision():
+    dec=int(input("would you like to view (1), display(2) ,register (3), or exit (4)"))
+    return dec
+              
+def check():
+    
 def main():
-    check()
+   
+    while True:
+        try:
+            
+            check()
+            if decision()==4:
+                break
+            elif deicion()==1:
+                view()
+            elif decision==2:
+                display()
+                
+            elif decision==3:
+                register()
+            
+            
+            
+            
+    
+        except ValueError:
+            print("h")
+            
+        
 
 if __name__ == "__main__":
     main()
